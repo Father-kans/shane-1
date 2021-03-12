5d536fa5f3cc0b6eda8fb7cc73de1913c72f9c4b
SangJo Kim <45070398+KimSangJo@users.noreply.github.com>
2021-03-12 13:29:46 +0900

    Update installer2.sh

--- selfdrive/assets/fonts/installer2.sh
+++ selfdrive/assets/fonts/installer2.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/bash
 
 mount -o remount,rw /system
-# install font
-cp -rf /data/openpilot/installer/fonts/NanumGothic* /system/fonts/
-# install font to /data/openpilot/selfdrive/assets/fonts/
-cp -rf /data/openpilot/installer/fonts/opensans_* /data/openpilot/selfdrive/assets/fonts/
-# install font mapping
-cp -rf /data/openpilot/installer/fonts/fonts.xml /system/etc/fonts.xml
+  	cp -f /data/openpilot/selfdrive/assets/fonts/opensans_* /system/fonts/
+    cp -f /data/openpilot/selfdrive/assets/fonts.xml /system/etc/fonts.xml
 # change permissions
 chmod 644 /system/etc/fonts.xml
 chmod 644 /system/fonts/NanumGothic*

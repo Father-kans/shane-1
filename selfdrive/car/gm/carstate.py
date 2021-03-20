aa2497cd55dc51a04f8ed97d0ab3b10f1ce83357
SangJo Kim <45070398+KimSangJo@users.noreply.github.com>
2021-03-20 10:59:24 +0900

    Enable EngineRPM

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -13,6 +13,7 @@ class CarState(CarStateBase):
     super().__init__(CP)
     can_define = CANDefine(DBC[CP.carFingerprint]['pt'])
     self.shifter_values = can_define.dv["ECMPRDNL"]["PRNDL"]
+    self.engineRPM = 0
 
   def update(self, pt_cp):
     ret = car.CarState.new_message()
@@ -46,6 +47,7 @@ class CarState(CarStateBase):
     # 0 inactive, 1 active, 2 temporarily limited, 3 failed
     self.lkas_status = pt_cp.vl["PSCMStatus"]['LKATorqueDeliveredStatus']
     ret.steerWarning = self.lkas_status not in [0, 1]
+    self.engineRPM = pt_cp.vl["ECMEngineStatus"]['EngineRPM']
 
     # 1 - open, 0 - closed
     ret.doorOpen = (pt_cp.vl["BCMDoorBeltStatus"]['FrontLeftDoor'] == 1 or
@@ -99,6 +101,7 @@ class CarState(CarStateBase):
       ("LKADriverAppldTrq", "PSCMStatus", 0),
       ("LKATorqueDelivered", "PSCMStatus", 0),
       ("LKATorqueDeliveredStatus", "PSCMStatus", 0),
+      ("EngineRPM", "ECMEngineStatus", 0),
       ("TractionControlOn", "ESPStatus", 0),
       ("EPBClosed", "EPBStatus", 0),
       ("CruiseMainOn", "ECMEngineStatus", 0),
